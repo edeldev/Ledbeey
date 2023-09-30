30eb148ca28a10c74d85afbcb9dcc03e2d1bc1ac
edeldev <edelsoto2502@gmail.com>
2023-09-29 19:04:16 -0600

    v1-ledbeey

--- src/app/(routes)/loading.jsx
+++ src/app/(routes)/loading.jsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="main bg__loader text-center">
-            <div class="lds-ripple"><div></div><div></div></div>
+            <div className="lds-ripple"><div></div><div></div></div>
         </div>
     )
 }
